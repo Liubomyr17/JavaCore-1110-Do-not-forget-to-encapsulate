327adc607ed0406af1a51ab157b63eab031338c4
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-28 09:09:27 +0200

    Add files via upload

---
+++ JavaCore 1110 Do not forget to encapsulate/src/com/company/Main.java
@@ -0,0 +1,74 @@
+package com.company;
+
+/*
+
+1110 Do not forget to encapsulate
+Look carefully at the methods and add the missing fields.
+Requirements:
+1. The Cat class must have a field of type String named name.
+2. The Cat class must have an int field named age.
+3. The Cat class must have an int field named weight.
+4. The Cat class must have an int field named speed.
+5. The Cat class must have 4 fields.
+6. All fields of the Cat class must be private.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
